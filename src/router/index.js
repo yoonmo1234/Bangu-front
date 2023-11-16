ae25aff03929e88adb5e50ebfd86be7f58e28841
yoonmo <yym4481@naver.com>
2023-11-16 11:54:35 +0900

    하던 프로젝트 옮기고 처음 시작

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: import("@/views/MainView.vue"),
+    },
+    {
+      path: "/board",
+      name: "board",
+      component: () => import("../views/TheBoardView.vue"),
+      redirect: { name: "article-list" },
+      children : [
+        {
+          path:"list",
+          name:"article-list",
+          component: () => import("@/components/board/BoardList.vue"),
+        },
+        {
+          path: "view/:articleno",
+          name: "article-view",
+          component: () => import("@/components/board/BoardDetail.vue"),
+        },
+        {
+          path: "write",
+          name: "article-write",
+          component: () => import("@/components/board/BoardWrite.vue"),
+        },
+        {
+          path: "modify/:articleno",
+          name: "article-modify",
+          component: () => import("@/components/board/BoardModify.vue"),
+        },
+      ]
+    }
+  ]
+});
+
+export default router
