98787c3923478b100c7822d3bc1d7ed8b1f1e360
yoonmo <yym4481@naver.com>
2023-11-16 11:54:35 +0900

    하던 프로젝트 옮기고 처음 시작

---
+++ src/components/ApartList.vue
@@ -0,0 +1,65 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="container p-4">
+		<div class="row">
+            <div class="col-md-9">
+	<div class="alert alert-success mt-3 text-center fw-bold" role="alert">
+		아파트 정보</div>
+	<!-- 아파트 매매 정보 검색 start -->
+	<form id = "form-search" method="GET" action="">
+		<input type="hidden" name="mode" value="search-dong">
+		<div class="row col-md-12 justify-content-center mb-2">
+			<div class="form-group col-md-2">
+				<select class="form-select bg-secondary text-light" id="sido" name="sido">
+					<option value="">시도선택</option>
+				</select>
+			</div>
+			<div class="form-group col-md-2">
+				<select class="form-select bg-secondary text-light" id="gugun" name="gugun">
+					<option value="">구군선택</option>
+				</select>
+			</div>
+			<div class="form-group col-md-2">
+				<select class="form-select bg-secondary text-light" id="dong" name="dong">
+					<option value="">동선택</option>
+				</select>
+			</div>
+			<div class="form-group col-md-2">
+				<select class="form-select bg-dark text-light" id="year" name="year"></select>
+			</div>
+			<div class="form-group col-md-2">
+				<select class="form-select bg-dark text-light" id="month" name="month">
+					<option value="">매매월선택</option>
+				</select>
+			</div>
+			<div class="form-group col-md-2">
+				<button type="button" id="list-btn" class="btn btn-outline-primary">
+					아파트매매정보</button>
+			</div>
+		</div>
+	</form>
+	<!-- kakao map start -->
+	<!-- <div id="map" class="mt-3" style="width: 100%; height: 400px"></div> -->
+	<!-- kakao map end -->
+	<!-- <c:choose> -->
+	<!-- <c:when test="${empty houseDealList}"> -->
+	<!-- <%@ include file="/apart/houseinfolist.jsp" %> -->
+	<!-- </c:when> -->
+	<!-- <c:when test="${empty houseList}"> -->
+	<!-- <%@ include file="/apart/housedeallist.jsp" %> -->
+	<!-- </c:when>
+	<c:otherwise>
+	<div></div>
+	</c:otherwise>
+	</c:choose> -->
+</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/board/BoardModify.vue
@@ -0,0 +1,24 @@
+<script setup>
+import BoardFormItem from "./item/BoardFormItem.vue";
+import {useRoute, useRouter} from "vue-router";
+const route =useRoute();
+
+const {articleNo} = route.params;
+</script>
+
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-10">
+        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+          <mark class="sky">글수정</mark>
+        </h2>
+      </div>
+      <div class="col-lg-10 text-start">
+        <BoardFormItem type="modify"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
