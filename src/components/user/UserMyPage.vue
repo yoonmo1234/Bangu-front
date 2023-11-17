6e82cf9fdbae81b5eb1c5cdb7a5e5de0c014d964
eoniedev <kwldjs1023@gmail.com>
2023-11-17 17:52:30 +0900

    feat: 유저 로그인 관련 컴포넌트 생성

---
+++ src/components/user/UserMyPage.vue
@@ -0,0 +1,26 @@
+<!-- 토큰 만료 테스트 페이지 -->
+<script setup>
+import { storeToRefs } from "pinia";
+import { useUserStore } from "@/stores/user";
+
+const storeUser = useUserStore();
+const { userInfo } = storeToRefs(storeUser);
+const { selectUserInfo } = storeUser;
+
+// Created
+// 8. 데이터 요청 (accessToken 이용)
+await selectUserInfo();
+</script>
+
+<template>
+  <b-container>
+    <b-row>
+      <b-col v-if="userInfo">
+        <h1>아이디: {{ userInfo.id }}</h1>
+        <h1>이름: {{ userInfo.name }}</h1>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<style scoped></style>
