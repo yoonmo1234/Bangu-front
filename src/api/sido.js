e85e975e5baaeb642327cfa64d7b7d5bf6824e4e
yoonmo1234 <yym4481@gmail.com>
2023-11-17 09:13:57 +0900

    feat:시도정보불러오기 완료

---
+++ src/api/sido.js
@@ -0,0 +1,23 @@
+import { sidoGugunDongAxios } from "../util/http-commons";
+
+// axios 객체
+const sidoGugunDong = sidoGugunDongAxios();
+
+let basicParam = {
+  regcode_pattern: "",
+  is_ignore_zero: true,
+};
+// 대한민국 시도 데이터를 가져와주는 api
+function listSido(success, fail) {
+  let param = {
+    regcode_pattern: "*00000000",
+    is_ignore_zero: true,
+  };
+  sidoGugunDong.get("", { params: param }).then(success).catch(fail);
+}
+// 대한민국 구군 데이터를 가져와주는 api
+function listGugun(param, success, fail) {
+  sidoGugunDong.get("", { params: param }).then(success).catch(fail);
+}
+
+export { listSido, listGugun };
