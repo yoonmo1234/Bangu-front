2937bd9aa0d41eb51d239be7136dc209eb923d0b
yang <yym4481@naver.com>
2023-11-19 17:48:17 +0900

    feat:아파트매매정보 불러오기, 마커표시

--- src/views/AptDealView.vue
+++ src/views/AptDealView.vue
@@ -2,6 +2,7 @@
 import VKakaoMap from "../components/common/VKakaoMap.vue";
 import { ref, onMounted, watch } from "vue";
 import { getAddress } from "@/api/sido";
+import { getApartDealInfo } from "@/api/apart";
 import VSelect from "@/components/common/VSelect.vue";
 
 const sidoList = ref([]);
@@ -19,6 +20,9 @@ const selectedGugun = ref({ text: "", code: "" });
 const selectedDong = ref({ text: "", code: "" });
 
 const currentLocation = ref({});
+const apartDealList = ref([]);
+
+const markerList = ref([]);
 
 onMounted(() => {
   getSidoList();
@@ -97,48 +101,158 @@ const dongChange = (key) => {
   selectedDong.value.code = key.value;
   selectedDong.value.text = key.text;
   getCurrentLocation();
+  getApartList();
+  setMarkerList();
 };
 
-const getCurrentLocation = () => {
+const getCurrentLocation = async () => {
   // console.log("asdasd",selectedDong.value.text);
   var geocoder = new kakao.maps.services.Geocoder();
-  geocoder.addressSearch(selectedDong.value.text, (result, status) => {
+  await geocoder.addressSearch(selectedDong.value.text, (result, status) => {
     if (status === kakao.maps.services.Status.OK) {
-      console.log(result);
-      const a = {
+      // console.log(result);
+      const curLatLng = {
         lat: result[0].y, // 위도
         lng: result[0].x, // 경도
       };
-      currentLocation.value = a;
+      currentLocation.value = curLatLng;
       // currentLocation.value.lat =  result[0].x;
       // currentLocation.value.lng =  result[0].y;
     }
   });
 };
+
+const getApartList = async () => {
+  await getApartDealInfo(
+    {
+      serviceKey: import.meta.env.VITE_OPEN_API_SERVICE_KEY,
+      LAWD_CD: selectedGugun.value.code.substring(0, 5), //앞의 5개가 지역코드임
+      DEAL_YMD: 202301,
+    },
+    ({ data }) => {
+      console.log("지역 코드 ", selectedGugun.value.code.substring(0, 5));
+      apartDealList.value = data.response.body.items.item;
+      console.log("apartDealList.value", apartDealList.value);
+      // setMarkerList();
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+};
+watch(apartDealList, () => {
+  // markerList.value.clear()];
+  markerList.value.splice(0, markerList.value.length);
+  console.log("markerList.value : ", markerList.value);
+  setMarkerList();
+});
+const setMarkerList = async () => {
+  const apts = [];
+  var geocoder = new kakao.maps.services.Geocoder();
+
+  for (const apt of apartDealList.value) {
+    const len = selectedDong.value.text.split(" ").length;
+    const dong = selectedDong.value.text.split(" ")[len - 1].trim();
+    if (apt["법정동"].trim() === dong) {
+      // geocoder.addressSearch(apt['도로명'].trim(), (result, status) => {
+      let add =
+        apt["중개사소재지"].trim() +
+        " " +
+        apt["법정동"].trim() +
+        " " +
+        apt["지번"];
+      console.log("add : ", add);
+      await geocoder.addressSearch(
+        add,
+        (result, status) => {
+          if (status === kakao.maps.services.Status.OK) {
+            // apts.push({
+            //     lat : result[0].y,
+            //     lng : result[0].x,
+            // });
+            // markerList.value = apts;
+            markerList.value.push({
+              lat: result[0].y,
+              lng: result[0].x,
+            });
+          }
+        },
+        {
+          analyze_type: "EXACT",
+        }
+      );
+    }
+  }
+
+  // if문으로 하나만 주었을 떄
+  // const apt = (apartDealList.value)[0]
+  // console.log("apt : " , apt);
+  //             let add = apt['중개사소재지'].trim() + " " + apt['법정동'].trim() + " " +  apt['지번'];
+  //             console.log("add : ", add);
+  //         await geocoder.addressSearch(add, (result, status) => {
+  //             if (status === kakao.maps.services.Status.OK) {
+  //                 apts.push({
+  //                     lat : result[0].y,
+  //                     lng : result[0].x,
+  //                 });
+  //                 markerList.value = apts;
+  //             }
+  //         },
+  //         {
+  //             analyze_type : 'EXACT',
+  //         });
+
+  //  apartDealList.value.forEach((apt) => {
+  //     const len = selectedDong.value.text.split(" ").length;
+  //     const dong = selectedDong.value.text.split(" ")[len-1].trim();
+  //     if(apt['법정동'].trim() === dong) {
+  //         // geocoder.addressSearch(apt['도로명'].trim(), (result, status) => {
+  //             let add = apt['중개사소재지'].trim() + " " + apt['법정동'].trim() + " " +  apt['지번'];
+  //             console.log("add : ", add);
+  //         geocoder.addressSearch(add, (result, status) => {
+  //             if (status === kakao.maps.services.Status.OK) {
+  //                 apts.push({
+  //                     lat : result[0].y,
+  //                     lng : result[0].x,
+  //                 });
+  //             }
+  //         },
+  //         {
+  //             analyze_type : 'EXACT',
+  //         });
+  //     }
+  // });
+  // markerList.value = apts;
+  // console.log("markerList.value",markerList.value)
+  // console.log("markerList에 변화 일어남");
+};
 </script>
 
 <template>
   <!-- <v-select @update:modelValue="sidoChange" label="시도정보" :items="sidoList" :item-title="'text'" :item-value="'value'"
-        variant="solo-filled"></v-select>
-    <v-select @update:modelValue="gugunChange" label="구군정보" :items="gugunList" :item-title="'text'" :item-value="'value'"
-        variant="solo-filled"></v-select>
-    <v-select @update:modelValue="dongChange" label="법정동정보" :items="dongList" :item-title="'text'" :item-value="'value'" variant="solo-filled"></v-select> -->
+            variant="solo-filled"></v-select>
+        <v-select @update:modelValue="gugunChange" label="구군정보" :items="gugunList" :item-title="'text'" :item-value="'value'"
+            variant="solo-filled"></v-select>
+        <v-select @update:modelValue="dongChange" label="법정동정보" :items="dongList" :item-title="'text'" :item-value="'value'" variant="solo-filled"></v-select> -->
 
   <VSelect :selectOption="sidoList" @onKeySelect="sidoChange" />
   <VSelect :selectOption="gugunList" @onKeySelect="gugunChange" />
   <VSelect :selectOption="dongList" @onKeySelect="dongChange" />
   <!-- <VSelect :selectOption="dongList"/> -->
   <!-- <select v-model="selected.sido" name="asdf" id="asdf">
-        <option  v-for="(list) in sidoList" :key="list.value" :value="list.value">{{ list.text }}</option>
-    </select>
-    <select name="asdf" id="asdf">
-        <option v-for="(list) in sidoList" :key="list.value" :value="list.value">{{ list.text }}</option>
-    </select>
-    <select name="asdf" id="asdf">
-        <option v-for="(list) in sidoList" :key="list.value" :value="list.value">{{ list.text }}</option>
-    </select> -->
+            <option  v-for="(list) in sidoList" :key="list.value" :value="list.value">{{ list.text }}</option>
+        </select>
+        <select name="asdf" id="asdf">
+            <option v-for="(list) in sidoList" :key="list.value" :value="list.value">{{ list.text }}</option>
+        </select>
+        <select name="asdf" id="asdf">
+            <option v-for="(list) in sidoList" :key="list.value" :value="list.value">{{ list.text }}</option>
+        </select> -->
 
-  <VKakaoMap :selectStation="currentLocation" />
+  <VKakaoMap
+    :currentLocation="currentLocation"
+    :markerList="markerList"
+    :apartDealList="apartDealList" />
 </template>
 
 <style scoped>
