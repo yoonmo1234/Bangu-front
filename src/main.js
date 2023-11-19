3e5d19f099700e458a7be1651eef368226a8c200
yang <yym4481@naver.com>
2023-11-20 01:09:07 +0900

    feat:houseStore로 이전
    값이 바로 전달 받지 못해 마커가 표시되지 않음
    소소한 에러가 많음

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
+import { createPinia } from 'pinia'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,7 +9,7 @@ import router from './router'
 const app = createApp(App)
 
 
-// app.use(createPinia())
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
