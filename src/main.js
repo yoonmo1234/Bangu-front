3ebe523388fc35db3e7d44da75589e0f14524e96
yoonmo1234 <yym4481@gmail.com>
2023-11-20 17:34:52 +0900

    feat:프론트 메인페이지 작업

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.vue'
 import router from './router'
 
-import '@/style.css';
+// import '@/style.css';
 
 
 // Vuetify
@@ -14,16 +12,16 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-const vuetify = createVuetify({
-    components,
-    directives,
-  })
+// const vuetify = createVuetify({
+//     components,
+//     directives,
+//   })
 
 const app = createApp(App)
 
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
+// app.use(vuetify)
 
 app.mount('#app')
