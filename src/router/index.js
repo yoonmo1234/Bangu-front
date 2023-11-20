54592bbec3e1c872e08b967d9ce1a5e95816023e
eoniedev <kwldjs1023@gmail.com>
2023-11-20 18:01:35 +0900

    feat: 라우터에 새로운 로그인 뷰 추가

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,11 @@ const router = createRouter({
       redirect: "/user/login",
       children: [
         {
+          path: "login-new",
+          name: "UserLoginNew",
+          component: () => import("@/components/user/UserLoginNew.vue"),
+        },
+        {
           path: "login",
           name: "UserLogin",
           component: () => import("@/components/user/UserLogin.vue"),
