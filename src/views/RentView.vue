e3569a1dc165ec851aab025e246fa9a03e3ec7c8
eoniedev <eoniedev@gmail.com>
2023-11-22 10:32:08 +0900

    feat: 충돌해결 전 커밋

--- src/views/RentView.vue
+++ src/views/RentView.vue
@@ -2,42 +2,39 @@
 import { ref, onMounted, watch } from "vue";
 
 // Store Import
-import {useRentStore } from '@/stores/rentStore';
-import {storeToRefs } from 'pinia';
+import { useRentStore } from "@/stores/rentStore";
+import { storeToRefs } from "pinia";
 
 // API Import
 import { getApartDealInfo } from "@/api/apart";
 
 // Component Import
-import VKakaoMap from '../components/common/VKakaoMap.vue';
+import VKakaoMap from "../components/common/VKakaoMap.vue";
 import HouseSearchBar from "../components/house/HouseSearchBar.vue";
 import VRentSearchBar from "../components/rent/VRentSearchBar.vue";
 
 //Store
 const rentStore = useRentStore();
 const {
-    selectedSido,
-    selectedGugun,
-    selectedDong,
-    // apartDealList,
-    // markerPositions,
+  selectedSido,
+  selectedGugun,
+  selectedDong,
+  // apartDealList,
+  // markerPositions,
 } = storeToRefs(rentStore);
 
 // const {getCurrentLocation} = houseStore;
 
-
 // Data
-
-
 
 let debounce = null;
 </script>
 
 <template>
-    <VRentSearchBar />
-    <!-- <VKakaoMap /> -->
-    <!-- <SideInfoModal /> -->
+  <RouterView></RouterView>
+  <!-- <VRentSearchBar /> -->
+  <!-- <VKakaoMap /> -->
+  <!-- <SideInfoModal /> -->
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
